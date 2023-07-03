f03bdc7a71bda8f43c592eaa19a831b77a3a2db2
JunYang <yangjun@raina.tech>
2023-07-03 10:31:33 +0800

    feat: create project class for current workspace

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,12 +1,28 @@
+import colors from 'colors';
+import { cosmiconfigSync } from 'cosmiconfig';
+import fse from 'fs-extra';
 import path from 'path';
 import { ProjectType } from '../types/project';
 
 export class Project {
   private _packageJsonDir: string;
-  private type: ProjectType;
+  private _config: { type: ProjectType };
 
   constructor(private workspace: string) {
     this._packageJsonDir = path.join(this.workspace, 'package.json');
+    const explorerResult = cosmiconfigSync('yawin', {
+      packageProp: 'yawinConfig',
+      stopDir: this.workspace,
+    }).search(this.workspace);
+
+    if (fse.existsSync(this._packageJsonDir)) {
+      console.log(colors.grey('[INFO]') + 'init config', explorerResult);
+      this._config = explorerResult?.config || {};
+    } else {
+      console.log(
+        colors.red('[ERROR]') + '当前目录不存在package.json文件: ' + colors.bgBlue(`${workspace}`),
+      );
+    }
   }
 }
 
